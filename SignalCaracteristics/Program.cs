6551dc5a1fd8a1321adcddfa87a98e49ce1b43ee
Adrian Acosta Mitjans <adrian.mitjans@gmail.com>
2019-06-08 16:20:51 -0700

    Magnitud y Desviación estandar

--- SignalCaracteristics/Program.cs
+++ SignalCaracteristics/Program.cs
@@ -9,59 +9,95 @@ namespace SignalCaracteristics
     {
         static void Main(string[] args)
         {
-            var files = Directory.GetFiles(System.IO.Directory.GetCurrentDirectory());
+            List<string> lines = new List<string>();
+            lines.Add("@RELATION activity");
+            lines.Add("");
+            lines.Add("@ATTRIBUTE s1mediaaccx\tREAL");
+            lines.Add("@ATTRIBUTE s1mediaaccy\tREAL");
+            lines.Add("@ATTRIBUTE s1mediaaccz\tREAL");
+            lines.Add("@ATTRIBUTE s1magxyz\tREAL");
+            lines.Add("@ATTRIBUTE s1sdmagxyz\tREAL");
+            lines.Add("@ATTRIBUTE s2mediaaccx\tREAL");
+            lines.Add("@ATTRIBUTE s2mediaaccy\tREAL");
+            lines.Add("@ATTRIBUTE s2mediaaccz\tREAL");
+            lines.Add("@ATTRIBUTE s2magxyz\tREAL");
+            lines.Add("@ATTRIBUTE s2sdmagxyz\tREAL");
+            lines.Add("@ATTRIBUTE class\t{caminando,subiendo-pendiente,bajando-pendiente,subiendo-escaleras,bajando-escaleras}");
+            lines.Add("");
+            lines.Add("@DATA");
+            
+            //Console.WriteLine("Actividad\n[1] caminando\n[2] subiendo-pendiente\n[3] bajando-pendiente\n[4]subiendo-escaleras\n[5] bajando-escaleras");
+            //var act = Console.ReadLine();
             //using (var reader = new StreamReader(@"C:\test.csv"))
-            foreach (var item in files)
+            var act = "";
+            foreach (string dir in Directory.GetDirectories(System.IO.Directory.GetCurrentDirectory()))
             {
-                if (item.Contains("csv"))
+                act = dir.Substring(System.IO.Directory.GetCurrentDirectory().Length + 1);
+                var files = Directory.GetFiles(dir);
+                foreach (var item in files)
                 {
-                    Console.WriteLine(Path.GetFileName(item));
-                    using (var reader = new StreamReader(item))
+                    if (item.Contains("csv"))
                     {
-                        List<float> x = new List<float>();
-                        List<float> y = new List<float>();
-                        List<float> z = new List<float>();
-                        var line = reader.ReadLine();
-                        var values = line.Split(',');
-                        while (!reader.EndOfStream)
+                        Console.WriteLine(Path.GetFileName(item));
+                        using (var reader = new StreamReader(item))
                         {
-                            line = reader.ReadLine();
-                            values = line.Split(',');
-
-                            x.Add(float.Parse(values[3], CultureInfo.InvariantCulture));
-                            y.Add(float.Parse(values[4], CultureInfo.InvariantCulture));
-                            z.Add(float.Parse(values[5], CultureInfo.InvariantCulture));
+                            List<float> s1x = new List<float>();
+                            List<float> s1y = new List<float>();
+                            List<float> s1z = new List<float>();
+                            List<float> s1mag = new List<float>();
+                            List<float> s2x = new List<float>();
+                            List<float> s2y = new List<float>();
+                            List<float> s2z = new List<float>();
+                            List<float> s2mag = new List<float>();
+                            var line = reader.ReadLine();
+                            var values = line.Split(',');
+                            while (!reader.EndOfStream)
+                            {
+                                line = reader.ReadLine();
+                                values = line.Split(',');
+                                if (int.Parse(values[0]) == 3)
+                                {
+                                    s1x.Add(float.Parse(values[3], CultureInfo.InvariantCulture));
+                                    s1y.Add(float.Parse(values[4], CultureInfo.InvariantCulture));
+                                    s1z.Add(float.Parse(values[5], CultureInfo.InvariantCulture));
+                                }
+                                else if (int.Parse(values[0]) == 4)
+                                {
+                                    s2x.Add(float.Parse(values[3], CultureInfo.InvariantCulture));
+                                    s2y.Add(float.Parse(values[4], CultureInfo.InvariantCulture));
+                                    s2z.Add(float.Parse(values[5], CultureInfo.InvariantCulture));
+                                }
+                            }
+                            s1mag = Magnitud(s1x, s1y, s1z);
+                            s2mag = Magnitud(s2x, s2y, s2z);
+                            lines.Add(FloatFormat(Media(s1x)) + "," + FloatFormat(Media(s1y)) + "," + FloatFormat(Media(s1z)) + "," + FloatFormat(Media(s1mag)) + "," + FloatFormat(SD(s1mag, Media(s1mag))) + "," + FloatFormat(Media(s2x)) + "," + FloatFormat(Media(s2y)) + "," + FloatFormat(Media(s2z)) + "," + FloatFormat(Media(s2mag)) + "," + FloatFormat(SD(s2mag, Media(s2mag))) + "," + act);
+                            Console.WriteLine();
                         }
-
-                        var mediax = Media(x);
-                        var mediay = Media(y);
-                        var mediaz = Media(z);
-
-                        Console.WriteLine("Media Eje x: " + mediax + ", SD: " + SD(x, mediax));
-                        Console.WriteLine("Media Eje y: " + mediay + ", SD: " + SD(y, mediay));
-                        Console.WriteLine("Media Eje z: " + mediaz + ", SD: " + SD(z, mediaz));
-
-                        var modax = Moda(x);
-                        var moday = Moda(y);
-                        var modaz = Moda(z);
-
-                        Console.WriteLine("Moda Eje x: " + modax[0].ToString() + " con " + modax[1].ToString() + " aparici" + (modax[1] > 1 ? "ones" : "ón"));
-                        Console.WriteLine("Moda Eje x: " + moday[0].ToString() + " con " + moday[1].ToString() + " aparici" + (moday[1] > 1 ? "ones" : "ón"));
-                        Console.WriteLine("Moda Eje x: " + modaz[0].ToString() + " con " + modaz[1].ToString() + " aparici" + (modaz[1] > 1 ? "ones" : "ón"));
-
-                        Max(x, "Maximo Eje x");
-                        Max(y, "Maximo Eje y");
-                        Max(z, "Maximo Eje z");
-
-                        Min(x, "Minimo Eje x");
-                        Min(y, "Minimo Eje y");
-                        Min(z, "Minimo Eje z");
-
-                        Console.WriteLine();
                     }
                 }
             }
-            Console.Read();
+            System.IO.File.WriteAllLines(System.IO.Directory.GetCurrentDirectory() + "\\actividad.arff", lines);
+        }
+
+        private static string FloatFormat(float value){
+            return value.ToString().Replace(",", ".");
+        }
+
+        private static string Actividad(int act)
+        {
+            switch (act)
+            {
+                case 1:
+                    return "caminando";
+                case 2:
+                    return "subiendo-pendiente";
+                case 3:
+                    return "bajando-pendiente";
+                case 4:
+                    return "subiendo-escaleras";
+                default:
+                    return "bajando-escaleras";
+            }
         }
 
         private static float Media(List<float> list)
@@ -72,7 +108,7 @@ namespace SignalCaracteristics
                 temp += item;
             }
             temp /= list.Count;
-            return (float) temp;
+            return (float)temp;
         }
 
         private static float[] Moda(List<float> list)
@@ -140,6 +176,16 @@ namespace SignalCaracteristics
             }
             temp /= list.Count;
             return MathF.Sqrt((float)temp);
+        }
+
+        private static List<float> Magnitud(List<float> x, List<float> y, List<float> z)
+        {
+            List<float> mag = new List<float>();
+            for (int i = 0; i < x.Count; i++)
+            {
+                mag.Add(MathF.Sqrt(MathF.Pow(x[i], 2) + MathF.Pow(y[i], 2) + MathF.Pow(z[i], 2)));
+            }
+            return mag;
         }
     }
 }
---
+++ SignalCaracteristics/SensorData.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SignalCaracteristics
+{
+    class SensorData
+    {
+        public List<float> x { get; set; }
+        public List<float> y { get; set; }
+        public List<float> z { get; set; }
+        public List<float> mag { get; set; }
+
+        private float mediax
+        {
+            get
+            {
+                var temp = 0.0;
+                foreach (var item in this.x)
+                {
+                    temp += item;
+                }
+                temp /= this.x.Count;
+                return (float)temp;
+            }
+        }
+        private float mediay
+        {
+            get
+            {
+                var temp = 0.0;
+                foreach (var item in this.y)
+                {
+                    temp += item;
+                }
+                temp /= this.y.Count;
+                return (float)temp;
+            }
+        }
+        private float mediaz
+        {
+            get
+            {
+                var temp = 0.0;
+                foreach (var item in this.z)
+                {
+                    temp += item;
+                }
+                temp /= this.z.Count;
+                return (float)temp;
+            }
+        }
+
+        public SensorData()
+        {
+            this.x = new List<float>();
+            this.y = new List<float>();
+            this.z = new List<float>();
+            this.mag = new List<float>();
+        }
+
+        public void AddX(float value) {
+            x.Add(value);
+        }
+        public void AddY(float value)
+        {
+            y.Add(value);
+        }
+        public void AddZ(float value)
+        {
+            z.Add(value);
+        }
+    }
+}
